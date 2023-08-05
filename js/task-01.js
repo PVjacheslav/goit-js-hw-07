e6df90dd9362ee135cc9a47dcb24749d027aac1c
PVjacheslav <128468517+PVjacheslav@users.noreply.github.com>
2023-08-05 13:26:43 +0300

    start

--- js/task-01.js
+++
@@ -1,8 +0,0 @@
-const listCategory = document.querySelectorAll(`.item`);
-// console.dir(numbCateg);
-console.log(`Number of categories: ${listCategory.length}`);
-
-listCategory.forEach(function(list) {
-    console.log(`Category:${list.firstElementChild.textContent}`);
-    console.log(`Elements:${list.lastElementChild.children.length}`);
-});
