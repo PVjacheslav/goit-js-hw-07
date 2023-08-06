37a88b4bd327488e55729292e7addd728247c7b0
PVjacheslav <128468517+PVjacheslav@users.noreply.github.com>
2023-08-06 08:54:46 +0300

    First tasck

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,45 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+// console.log(basicLightbox);
+
+const container = document.querySelector('.gallery');
+
+function createMarkup (arr) {
+    return arr.map(({ preview, original, description }) =>
+    `<li class = "gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+    </a>
+    </li>`
+    ).join('')
+}
+
+container.insertAdjacentHTML('beforeend', createMarkup(galleryItems));
+container.addEventListener('click', handlerGalleryClick);
+function handlerGalleryClick(evt) {
+    evt.preventDefault();
+    if(!evt.target.classList.contains('gallery__image')) {
+        return;
+    }
+    // console.log(evt.target);
+    const picture = evt.target.dataset.sourse;
+    const instance = basicLightbox.create (
+    `<div class="modal"><img src="${picture}" alt=""></div>`
+    )
+    instance.show();
+
+    container.addEventListener('keydown', (evt) => {
+        if (evt.code === 'Escape') {
+            instance.close();
+        }
+    });
+}
+
+console.log(galleryItems)
